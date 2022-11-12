2b2cc7686d1c6cee8d6c8be81af88f9d6fe5e6f8
Manisha <mpaswale@gmail.com>
2022-11-12 13:34:29 +0530

    Check new files added

---
+++ src/main/java/com/jdc/WebDeploy1Application.java
@@ -0,0 +1,13 @@
+package com.jdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDeploy1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDeploy1Application.class, args);
+	}
+
+}
